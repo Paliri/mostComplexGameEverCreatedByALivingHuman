75b4197850662bf919f6f03dc22fde22598d617a
palitay.m <m.titanium@titanium.im>
2019-04-11 14:24:43 +0300

    fix. Part 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,38 +26,7 @@ public class Main {
                 System.out.println("Мистика - твое всё");
                 break;
             case 4:
-                System.out.println("Пир-пир-пир!! Ты - лучник! " + "\n" +
-                        "                    .;;,.    \n" +
-                        "                  ; '\" ;\\ \\//\n" +
-                        "                 \\|a (a|7 \\//\n" +
-                        "                 j| ..  | ||/\n" +
-                        "                //'.--.')\\-,/\n" +
-                        "              .-||- '' ||/  `-.\n" +
-                        "             ;  | \\ |/ |/ L.  ,|\n" +
-                        "             f\\ |\\| Y  || \\ '._\\\n" +
-                        "            j | \\|     (| |   | |\n" +
-                        "           |  L_\\         L.__: |\n" +
-                        "            \\(  '-.,-,    |   ; |\n" +
-                        "             |'-.'.L_rr>  f--f  |\n" +
-                        ".-=,,______,--------- J-. ;  ;__ \n" +
-                        "   ``\"-,__   |  |      h  |  f  '--.__\n" +
-                        "       `--;;--,_       h  f-j   |   __;==-.\n" +
-                        "            / `-''-,,__J,'  \\_..--:'-'     '\n" +
-                        "            | |    `' --L7//'-'`|\n" +
-                        "            | ,     ||  h    |  (\n" +
-                        "            | ;     | \\ J    j   |\n" +
-                        "            | L__   | |  L_.'    |\n" +
-                        "            |   |'-.| L.'h  |  : |\n" +
-                        "            |;  \\     |  J ; : : |\n" +
-                        "            | :  (    \\  'L| : : |\n" +
-                        "            | ;   \\'.--|    \\  : |\n" +
-                        "            | | : \\    \\-, /`\\ : |\n" +
-                        "            L-'-;__\\   \\\\ '  | | |\n" +
-                        "                    ;   \\\\   |'L_j\n" +
-                        "                    _>  _|   |\n" +
-                        "             snd   <___/ /-  \\\n" +
-                        "                        /    /\n" +
-                        "                        '---'");
+                System.out.println("Пир-пир-пир!! Ты - лучник!");
                 break;
             case 5:
                 System.out.println("В твоем арсенале молитвы и вера в лучшее :D");
@@ -70,27 +39,26 @@ public class Main {
     }
 
     private static void ask() {
-        System.out.println("Как я могу обращаться к тебе, смерд?");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Приветствую тебя, " + name + "!");
-        while (true) {
-            if (choice()) break;
-        }
-        askRepeat();
+        do {
+            System.out.println("Как я могу обращаться к тебе, смерд?");
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            System.out.println("Приветствую тебя, " + name + "!");
+            while (!choice()) {};
+        } while(askRepeat());
     }
 
-    private static void askRepeat() {
-        System.out.println("Вы хотите повторить, всё это еще разок? YES or NO?");
-        Scanner scanner = new Scanner(System.in);
-        String answer = scanner.nextLine().toUpperCase();
-        if ("YES".equals(answer)) {
-            ask();
-        } else if ("NO".equals(answer)) {
-            System.out.println("Ну и ладно! До встречи!");
-        } else {
-            askRepeat();
+    private static boolean askRepeat() {
+        do{
+            System.out.println("Вы хотите повторить, всё это еще разок? Пидр, YES or NO?");
+            Scanner scanner = new Scanner(System.in);
+            String answer = scanner.nextLine().toUpperCase();
+                if ("YES".equals(answer)) {
+                    return true;
+                } else if ("NO".equals(answer)) {
+                    return false;
+                }
+            }while(true);
         }
 
     }
-}
